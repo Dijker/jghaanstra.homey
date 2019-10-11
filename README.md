d741259882d0aa5eff0dbedb5279c8ca44096325
jghaanstra <jghaanstra@gmail.com>
2019-10-12 00:08:03 +0200

    Get data from devices using Web API

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 - Personal LED-Ring Collection (based on https://github.com/OpenMindNL/ledcollection)
 - Speech Parser (based on https://github.com/irritanterik/homey-parse-speech)
 - Text to speech from external POST request (used for Home Assistant)
+- Get data from devices using external POST request (used from KLWP)
 - Zwave support for Düwi Built-in Switch ZW ES 1000 / Zwave.Me 05431
 - Action card for Weerlive.nl daily forecast
--- api.js
+++ api.js
@@ -1,14 +1,29 @@
 const Homey = require('homey');
+const util = require('/lib/utils.js');
 
 module.exports = [
 	{
 		description: 'Say text from POST request',
 		method   : 'POST',
-		path     : '/:source',
+		path     : '/say/:source',
 		public   : true,
 		fn: function(args, callback) {
     	Homey.ManagerSpeechOutput.say(args.body.message);
       return callback(null, 'OK');
+		}
+	},
+  {
+		description: 'Return data from devices',
+		method   : 'POST',
+		path     : '/owneyapi',
+		public   : true,
+		fn: async function(args, callback) {
+      try {
+        const data = await util.getData(args.body.devices.split(", "));
+        return callback(null, data);
+      } catch (error) {
+        return callback(error, false);
+      }
 		}
 	}
 ]
